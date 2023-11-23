f3775d8c4d4946b83894ba2016e47fb609af916d
max-ajwang <maxajwang@gmail.com>
2023-11-23 23:33:21 +0300

    Dark Theme Toggle Design and Context API

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+'use client';
+import React, { useContext } from 'react';
 import styles from './themeToggle.module.css';
+import Image from 'next/image';
+import { ThemeContext } from '@/context/ThemeContext';
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>;
+  const { toggle, theme } = useContext(ThemeContext);
+  console.log(theme);
+  return (
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === 'dark' ? { background: 'white' } : { background: '#0f172a' }
+      }
+    >
+      <Image src="/moon.png" alt="" width={14} height={14} />
+      <div
+        className={styles.ball}
+        style={
+          theme === 'dark'
+            ? { left: 1, background: '#0f172a' }
+            : { right: 1, background: 'white' }
+        }
+      ></div>
+      <Image src="/sun.png" alt="" width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
