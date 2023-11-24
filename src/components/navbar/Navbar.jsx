51be9e5d949916b39cb3898cdc184aa30f5b46b1
max-ajwang <maxajwang@gmail.com>
2023-11-24 12:55:29 +0300

    Next.js responsive hamburger menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,12 +14,18 @@ const Navbar = () => {
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>Max Ajwang'</div>
+      <div className={styles.logo}>MaxTheCTO</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/">Homepage</Link>
-        <Link href="/">About</Link>
-        <Link href="/">Contact</Link>
+        <Link href="/" className={styles.link}>
+          Homepage
+        </Link>
+        <Link href="/" className={styles.link}>
+          About
+        </Link>
+        <Link href="/" className={styles.link}>
+          Contact
+        </Link>
         <AuthLinks />
       </div>
     </div>
